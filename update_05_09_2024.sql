45843089e57fe22043751379574949e205fc4161
Dương Doãn Phú <85473038+DDPhu0810@users.noreply.github.com>
2024-09-05 11:43:43 +0700

    Add files via upload

---
+++ update_05_09_2024.sql
@@ -0,0 +1,201 @@
+use billing_server;
+
+UPDATE `billing_server_test`.`config` SET `id`='63' WHERE  `id`=62;
+UPDATE `billing_server_test`.`config` SET `id`='62' WHERE  `id`=61;
+UPDATE `billing_server_test`.`config` SET `id`='61' WHERE  `id`=60;
+UPDATE `billing_server_test`.`config` SET `id`='60' WHERE  `id`=59;
+UPDATE `billing_server_test`.`config` SET `id`='59' WHERE  `id`=58;
+UPDATE `billing_server_test`.`config` SET `id`='58' WHERE  `id`=57;
+UPDATE `billing_server_test`.`config` SET `id`='57' WHERE  `id`=56;
+UPDATE `billing_server_test`.`config` SET `id`='56' WHERE  `id`=55;
+UPDATE `billing_server_test`.`config` SET `id`='55' WHERE  `id`=54;
+UPDATE `billing_server_test`.`config` SET `id`='54' WHERE  `id`=53;
+UPDATE `billing_server_test`.`config` SET `id`='53' WHERE  `id`=52;
+UPDATE `billing_server_test`.`config` SET `id`='52' WHERE  `id`=51;
+UPDATE `billing_server_test`.`config` SET `id`='51' WHERE  `id`=50;
+
+UPDATE `billing_server_test`.`config` SET `id`='50' WHERE  `id`=49;
+UPDATE `billing_server_test`.`config` SET `id`='49' WHERE  `id`=48;
+UPDATE `billing_server_test`.`config` SET `id`='48' WHERE  `id`=47;
+UPDATE `billing_server_test`.`config` SET `id`='47' WHERE  `id`=46;
+UPDATE `billing_server_test`.`config` SET `id`='46' WHERE  `id`=45;
+UPDATE `billing_server_test`.`config` SET `id`='45' WHERE  `id`=44;
+UPDATE `billing_server_test`.`config` SET `id`='44' WHERE  `id`=43;
+UPDATE `billing_server_test`.`config` SET `id`='43' WHERE  `id`=42;
+UPDATE `billing_server_test`.`config` SET `id`='42' WHERE  `id`=41;
+UPDATE `billing_server_test`.`config` SET `id`='41' WHERE  `id`=40;
+
+UPDATE `billing_server_test`.`config` SET `id`='40' WHERE  `id`=39;
+UPDATE `billing_server_test`.`config` SET `id`='39' WHERE  `id`=38;
+UPDATE `billing_server_test`.`config` SET `id`='38' WHERE  `id`=37;
+UPDATE `billing_server_test`.`config` SET `id`='37' WHERE  `id`=36;
+UPDATE `billing_server_test`.`config` SET `id`='36' WHERE  `id`=35;
+UPDATE `billing_server_test`.`config` SET `id`='35' WHERE  `id`=34;
+UPDATE `billing_server_test`.`config` SET `id`='34' WHERE  `id`=33;
+UPDATE `billing_server_test`.`config` SET `id`='33' WHERE  `id`=32;
+UPDATE `billing_server_test`.`config` SET `id`='32' WHERE  `id`=31;
+UPDATE `billing_server_test`.`config` SET `id`='31' WHERE  `id`=30;
+
+UPDATE `billing_server_test`.`config` SET `id`='30' WHERE  `id`=29;
+UPDATE `billing_server_test`.`config` SET `id`='29' WHERE  `id`=28;
+UPDATE `billing_server_test`.`config` SET `id`='28' WHERE  `id`=27;
+UPDATE `billing_server_test`.`config` SET `id`='27' WHERE  `id`=26;
+UPDATE `billing_server_test`.`config` SET `id`='26' WHERE  `id`=25;
+UPDATE `billing_server_test`.`config` SET `id`='25' WHERE  `id`=24;
+UPDATE `billing_server_test`.`config` SET `id`='24' WHERE  `id`=23;
+UPDATE `billing_server_test`.`config` SET `id`='23' WHERE  `id`=22;
+UPDATE `billing_server_test`.`config` SET `id`='22' WHERE  `id`=21;
+UPDATE `billing_server_test`.`config` SET `id`='21' WHERE  `id`=20;
+
+UPDATE `billing_server_test`.`config` SET `id`='20' WHERE  `id`=19;
+UPDATE `billing_server_test`.`config` SET `id`='19' WHERE  `id`=18;
+UPDATE `billing_server_test`.`config` SET `id`='18' WHERE  `id`=17;
+UPDATE `billing_server_test`.`config` SET `id`='17' WHERE  `id`=16;
+UPDATE `billing_server_test`.`config` SET `id`='16' WHERE  `id`=15;
+UPDATE `billing_server_test`.`config` SET `id`='15' WHERE  `id`=14;
+UPDATE `billing_server_test`.`config` SET `id`='14' WHERE  `id`=13;
+UPDATE `billing_server_test`.`config` SET `id`='13' WHERE  `id`=12;
+UPDATE `billing_server_test`.`config` SET `id`='12' WHERE  `id`=11;
+UPDATE `billing_server_test`.`config` SET `id`='11' WHERE  `id`=10;
+
+UPDATE `billing_server_test`.`config` SET `id`='10' WHERE  `id`=9;
+UPDATE `billing_server_test`.`config` SET `id`='9' WHERE  `id`=8;
+UPDATE `billing_server_test`.`config` SET `id`='8' WHERE  `id`=7;
+UPDATE `billing_server_test`.`config` SET `id`='7' WHERE  `id`=6;
+UPDATE `billing_server_test`.`config` SET `id`='6' WHERE  `id`=5;
+UPDATE `billing_server_test`.`config` SET `id`='5' WHERE  `id`=4;
+UPDATE `billing_server_test`.`config` SET `id`='4' WHERE  `id`=3;
+UPDATE `billing_server_test`.`config` SET `id`='3' WHERE  `id`=2;
+UPDATE `billing_server_test`.`config` SET `id`='2' WHERE  `id`=1;
+
+
+INSERT INTO `billing_server_test`.`price_config` (`id`, `gateway_id`, `operator`, `type`, `price_minute`) VALUES ('1', 'PREFIX_1900', 'PREFIX_1900', 'prefix_1900', '1500');
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Dumping structure for procedure billing_server_test.proc_inbound_call_histories_4_admin
+DROP PROCEDURE IF EXISTS `proc_inbound_call_histories_4_admin`;
+DELIMITER //
+CREATE PROCEDURE `proc_inbound_call_histories_4_admin`(
+	IN `p_startTime` DATETIME,
+	IN `p_endTime` DATETIME,
+	IN `p_usernames` LONGTEXT,
+	IN `p_groupnames` LONGTEXT,
+	IN `p_gatewaynames` LONGTEXT
+)
+BEGIN
+
+SELECT user_name, display_name, from_phone_number, dest_device_name, department, to_phone_number, SUM(duration),
+		 CEIL(SUM(duration)/60) * (SELECT price_minute FROM price_config WHERE `type` = 'prefix_1900') AS cost, min(called_at)
+FROM call_histories_cdr
+WHERE LENGTH(calling_party_number) > 9 AND calling_party_number NOT LIKE 'b%' AND duration > 0
+		AND called_at >= case when p_startTime IS NULL then called_at else p_startTime END 
+      AND called_at <=  case when p_endTime IS NULL then called_at else p_endTime END 
+      AND (case when COALESCE(p_usernames, 'ALL') = 'ALL' then 1 when COALESCE(p_usernames, 'ALL')<> 'ALL' and FIND_IN_SET(user_name,p_usernames) then 1 else 0 end) = 1 
+      AND (case when COALESCE(p_groupnames, 'ALL') = 'ALL' then 1 when COALESCE(p_groupnames, 'ALL')<> 'ALL' and FIND_IN_SET(department,p_groupnames)then 1 else 0 end) = 1
+      AND (case when COALESCE(p_gatewaynames, 'ALL') = 'ALL' then 1 when COALESCE(p_gatewaynames, 'ALL')<> 'ALL' and FIND_IN_SET(dest_device_name,p_gatewaynames) then 1 else 0 end) = 1
+		AND FIND_IN_SET(to_phone_number, (SELECT value_config FROM `config` WHERE key_config = 'IVR_prefix_1900')) AND in_out = "Inbound"
+GROUP BY orig_leg_call_identifier;
+                    
+END//
+DELIMITER ;
+
+-- Dumping structure for procedure billing_server_test.proc_inbound_call_histories_4_leader
+DROP PROCEDURE IF EXISTS `proc_inbound_call_histories_4_leader`;
+DELIMITER //
+CREATE PROCEDURE `proc_inbound_call_histories_4_leader`(
+	IN `p_users_condition` LONGTEXT,
+	IN `p_startTime` DATETIME,
+	IN `p_endTime` DATETIME,
+	IN `p_usernames` LONGTEXT,
+	IN `p_groupnames` LONGTEXT,
+	IN `p_gatewaynames` LONGTEXT
+)
+BEGIN
+
+SELECT user_name, display_name, from_phone_number, dest_device_name, department, to_phone_number, SUM(duration),
+		 CEIL(SUM(duration)/60) * (SELECT price_minute FROM price_config WHERE `type` = 'prefix_1900') AS cost, min(called_at)
+FROM call_histories_cdr
+WHERE LENGTH(calling_party_number) > 9 AND calling_party_number NOT LIKE 'b%' AND duration > 0
+		AND called_at >= case when p_startTime IS NULL then called_at else p_startTime END 
+      AND called_at <=  case when p_endTime IS NULL then called_at else p_endTime END 
+      AND (case when COALESCE(p_usernames, 'ALL') = 'ALL' then 1 when COALESCE(p_usernames, 'ALL')<> 'ALL' and FIND_IN_SET(user_name,p_usernames) then 1 else 0 end) = 1 
+      AND (case when COALESCE(p_groupnames, 'ALL') = 'ALL' then 1 when COALESCE(p_groupnames, 'ALL')<> 'ALL' and FIND_IN_SET(department,p_groupnames) then 1 else 0 end) = 1
+      AND (case when COALESCE(p_gatewaynames, 'ALL') = 'ALL' then 1 when COALESCE(p_gatewaynames, 'ALL')<> 'ALL' and FIND_IN_SET(dest_device_name,p_gatewaynames) then 1 else 0 end) = 1
+		AND FIND_IN_SET(c.userName,p_users_condition)
+		AND FIND_IN_SET(to_phone_number, (SELECT value_config FROM `config` WHERE key_config = 'IVR_prefix_1900')) AND in_out = "Inbound"
+GROUP BY orig_leg_call_identifier;
+                    
+END//
+DELIMITER ;
+
+-- Dumping structure for procedure billing_server_test.proc_inbound_call_histories_4_view_all
+DROP PROCEDURE IF EXISTS `proc_inbound_call_histories_4_view_all`;
+DELIMITER //
+CREATE PROCEDURE `proc_inbound_call_histories_4_view_all`(
+	IN `p_startTime` DATETIME,
+	IN `p_endTime` DATETIME,
+	IN `p_usernames` LONGTEXT,
+	IN `p_groupnames` LONGTEXT,
+	IN `p_gatewaynames` LONGTEXT,
+	IN `p_ipphone` LONGTEXT
+)
+BEGIN
+
+SELECT user_name, calling_party_number, dest_device_name, department, to_phone_number, SUM(duration), 
+		 CEIL(SUM(duration)/60) * (SELECT price_minute FROM price_config WHERE `type` = 'prefix_1900') AS cost, min(called_at)
+FROM call_histories_cdr
+WHERE LENGTH(calling_party_number) > 9 AND calling_party_number NOT LIKE 'b%' AND duration > 0
+		AND called_at >= case when p_startTime IS NULL then called_at else p_startTime END 
+      AND called_at <=  case when p_endTime IS NULL then called_at else p_endTime END 
+      AND (case when COALESCE(p_usernames, 'ALL') = 'ALL' then 1 when COALESCE(p_usernames, 'ALL')<> 'ALL' and FIND_IN_SET(user_name,p_usernames) then 1 else 0 end) = 1 
+      AND (case when COALESCE(p_groupnames, 'ALL') = 'ALL' then 1 when COALESCE(p_groupnames, 'ALL')<> 'ALL' and FIND_IN_SET(department,p_groupnames) then 1 else 0 end) = 1
+      AND (case when COALESCE(p_gatewaynames, 'ALL') = 'ALL' then 1 when COALESCE(p_gatewaynames, 'ALL')<> 'ALL' and FIND_IN_SET(dest_device_name,p_gatewaynames) then 1 else 0 end) = 1
+		AND (to_phone_number = p_ipphone)
+		AND FIND_IN_SET(to_phone_number, (SELECT value_config FROM `config` WHERE key_config = 'IVR_prefix_1900')) AND in_out = "Inbound"
+GROUP BY orig_leg_call_identifier;
+                    
+END//
+DELIMITER ;
+
+-- Dumping structure for procedure billing_server_test.proc_inbound_call_histories_4_view_all_with_gateway_manager
+DROP PROCEDURE IF EXISTS `proc_inbound_call_histories_4_view_all_with_gateway_manager`;
+DELIMITER //
+CREATE PROCEDURE `proc_inbound_call_histories_4_view_all_with_gateway_manager`(
+	IN `p_startTime` DATETIME,
+	IN `p_endTime` DATETIME,
+	IN `p_usernames` LONGTEXT,
+	IN `p_groupnames` LONGTEXT,
+	IN `p_gatewaynames` LONGTEXT,
+	IN `p_ipphone` LONGTEXT,
+	IN `p_gateway_managers` LONGTEXT
+)
+BEGIN
+
+SELECT user_name, calling_party_number, dest_device_name, department, to_phone_number, SUM(duration), 
+		 CEIL(SUM(duration)/60) * (SELECT price_minute FROM price_config WHERE `type` = 'prefix_1900') AS cost, min(called_at)
+FROM call_histories_cdr
+WHERE LENGTH(calling_party_number) > 9 AND calling_party_number NOT LIKE 'b%' AND duration > 0
+		AND called_at >= case when p_startTime IS NULL then called_at else p_startTime END 
+      AND called_at <=  case when p_endTime IS NULL then called_at else p_endTime END 
+      AND (case when COALESCE(p_usernames, 'ALL') = 'ALL' then 1 when COALESCE(p_usernames, 'ALL')<> 'ALL' and FIND_IN_SET(user_name,p_usernames) then 1 else 0 end) = 1 
+      AND (case when COALESCE(p_groupnames, 'ALL') = 'ALL' then 1 when COALESCE(p_groupnames, 'ALL')<> 'ALL' and FIND_IN_SET(department,p_groupnames) then 1 else 0 end) = 1
+      AND (case when COALESCE(p_gatewaynames, 'ALL') = 'ALL' then 1 when COALESCE(p_gatewaynames, 'ALL')<> 'ALL' and FIND_IN_SET(dest_device_name,p_gatewaynames) then 1 else 0 end) = 1
+		AND (to_phone_number = p_ipphone OR FIND_IN_SET(dest_device_name,p_gateway_managers))
+		AND FIND_IN_SET(to_phone_number, (SELECT value_config FROM `config` WHERE key_config = 'IVR_prefix_1900')) AND in_out = "Inbound"
+GROUP BY orig_leg_call_identifier;
+                    
+END//
+DELIMITER ;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+
